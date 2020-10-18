d93f0e6e5753211135f50487f9c79355104b2fef
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2020-10-18 18:49:15 -0300

    backend web mobile

---
+++ backend/src/database/migrations/1603046925551-users.ts
@@ -0,0 +1,37 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { number } from "yup";
+
+export class users1603046925551 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                }, 
+                {
+                name: 'password',
+                type: 'varchar',}
+            ],
+            
+    }))
+    
+    }
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+
+    }
+
+}
